8e6b36839a0f411ca7d66e55b399e6b26c957837
tarball0 <chrispaul.tar.gz@proton.me>
2024-12-03 21:49:04 +0530

    day-3 p2 completed
    programming in rust is like trying to cook with your arms cut off.

--- 2024/day-3/day-3.rs
+++ 2024/day-3/day-3.rs
@@ -1,26 +1,45 @@
+// Solution 1:
 // grep -Po 'mul\((\d{1,3}),(\d{1,3})\)' input.txt > parsed-input.txt
 // sed -i -e 's/mul(//g' parsed-input.txt
 // sed -i -e 's/)//g' parsed-input.txt
 // sed -i -e 's/,/ /g' parsed-input.txt
+//
+// solution 2:
+// grep -Po "(mul\\((\\d+),(\\d+)\\))|(do\\(\\))|(don't\\(\\))" input.txt.bak > parsed-input2.txt
+// repeat 'sed' steps from lines 2,3,4
+// sed -i -e 's/(//g' parsed-input.txt
+//
+// could've made a bash script for this whole thing, but honestly, couldn't be arsed.
 
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
 fn main() {
-    let file = File::open("parsed-input.txt").unwrap();
+    let file = File::open("parsed-input2.txt").unwrap();
     let reader = BufReader::new(file);
     let mut sum = 0;
+    let mut dont = false;
 
     for line in reader.lines() {
         let line = line.unwrap();
+        if line == "don't" {
+            dont = true;
+            continue;
+        } else if line == "do" {
+            dont = false;
+            continue;
+        }
+
+        if !dont {
         let parts = line.split(" ");
         let mut product: i32 = 1;
         for part in parts {
             product *= part.parse::<i32>().unwrap();
         }
         sum += product;
+        }
     }
 
-    println!("answer 1: {}", sum);
+    println!("answer: {}", sum);
 
 }
